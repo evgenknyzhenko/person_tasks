2d7974e56079def6247b5be9da6d7b5066668617
yevgen <evgen.knyzhenko@gmail.com>
2026-05-02 21:50:50 +0300

    Reformat code

--- src/main/java/com/example/person_tasks/PersonTasksApplication.java
+++ src/main/java/com/example/person_tasks/PersonTasksApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonTasksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonTasksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonTasksApplication.class, args);
+    }
 
 }
