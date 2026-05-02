379793a49af3bd97ebd76b5523abe7ad7c4d7983
yevgen <evgen.knyzhenko@gmail.com>
2026-05-02 18:19:51 +0300

    Initial commit

---
+++ src/main/java/com/example/person_tasks/PersonTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.person_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/person_tasks/PersonTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.person_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
